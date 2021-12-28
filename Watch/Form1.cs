64f9dd699f4902db81f4982cd337a5606065e715
BaeJuhwan <bjw505@naver.com>
2021-12-28 23:18:03 +0900

    first commit

---
+++ Watch/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Timer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Interval = 1000;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // 1초마다 현재시간을 갱신한다.
+            lblTime.Text = System.DateTime.Now.ToString("yyyy-MM-dd  hh:mm:ss");
+        }
+    }
+}
